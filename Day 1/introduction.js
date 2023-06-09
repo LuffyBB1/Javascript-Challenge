43da2d09daf2364f5363e567f2476b7392e60595
Brayan Berna <brayan.bernal@sasnw.com>
2023-06-09 11:17:08 -0500

    Clase dia 1

---
+++ Day 1/introduction.js
@@ -0,0 +1,18 @@
+console.log('Welcome to 30DaysOfJavaScript')
+let myAge=25
+let yourAge=21
+console.log("I'm",myAge, "years old." )
+console.log("You are",yourAge, "years old." )
+
+// commenting the code itself with a single comment
+// let firstName = 'Asabeneh'; single line comment
+// let lastName = 'Yetayeh'; single line comment
+
+
+/*
+  let location = 'Helsinki';
+  let age = 100;
+  let isMarried = true;
+  This is a Multiple line comment
+*/
+
